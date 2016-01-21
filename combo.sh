5eb51b90e2d1de3e57bc6524a7405b0ae61c79f3
Christopher Getschmann <christopher@volzo.de>
2016-01-22 00:14:39 +0100

    comments

--- combo.sh
+++ combo.sh
@@ -1,7 +1,9 @@
 #/bin/sh
-
-python3 scraper.py              #                   --> raw.pickle
-python3 weighter.py             # raw.pickle        --> weighted.pickle
-sh ./extract_streets.sh         # xapi_export.osm   --> streets.csv
-python3 streetconverter.py      # streets.csv       --> streets.json
-python3 merger.py               # weighted.pickle / streets.json --> merged_output.json
+                                # input                         --> output
+                                
+python3 scraper.py              #                               --> raw.pickle
+python3 weighter.py             # raw.pickle                    --> weighted.pickle
+sh ./extract_streets.sh         # xapi_export.osm               --> streets.csv
+python3 streetconverter.py      # streets.csv                   --> streets.json
+python3 merger.py               # weighted.pickle, streets.json --> merged_output.json
+python3 kmlgenerator.py         # merged_output.json            --> output.kml
